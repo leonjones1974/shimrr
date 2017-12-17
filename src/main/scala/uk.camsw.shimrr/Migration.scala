9bc70c67014b3aa570681b21ebb5e5e84191f637
Leon Jones <leonj_24@yahoo.com>
2017-12-17 13:20:44 +0000

    Tidy up a bit

--- src/main/scala/uk.camsw.shimrr/Migration.scala
+++ src/main/scala/uk.camsw.shimrr/Migration.scala
@@ -26,40 +26,33 @@ object Migration {
     def migrateTo[B](implicit migration: Migration[A, B]): GenSeq[B] = xs map migration.migrate
   }
 
-  
+
   implicit def cnilMigration[T <: Coproduct, B, BRepr](implicit
                                                        genB: LabelledGeneric.Aux[B, BRepr]
                                                       ): Migration[T, B] =
-    Migration.lift(_ => {
-      throw new RuntimeException("Will not happen")
-    })
+    Migration.lift(a =>
+      throw new RuntimeException(s"Will not happen, but did for $a")
+    )
 
-  implicit def coproductMigration[H, T <: Coproduct, B, BRepr <: HList](implicit
-                                                                        genB: LabelledGeneric.Aux[B, BRepr],
-                                                                        mH: Migration[H, B],
-                                                                        mT: Migration[T, B]
-                                                                       ): Migration[H :+: T, B] =
+  implicit def coproductReprMigration[H, T <: Coproduct, B, BRepr <: HList](implicit
+                                                                            genB: LabelledGeneric.Aux[B, BRepr],
+                                                                            mH: Migration[H, B],
+                                                                            mT: Migration[T, B]
+                                                                           ): Migration[H :+: T, B] =
     Migration.lift {
       case Inl(h) =>
-        println(s"Migrating using coproduct head: $h")
-        val migrated = mH.migrate(h)
-        println(s"Migrated coprod $h => $migrated")
-        migrated
+        mH.migrate(h)
       case Inr(t) =>
-        println(s"Migrating using coproduct tail: $t")
         mT.migrate(t)
     }
 
-  implicit def genericMigration[A, B, ARepr <: Coproduct, BRepr <: HList](implicit
-                                                                          genA: Generic.Aux[A, ARepr],
-                                                                          genB: LabelledGeneric.Aux[B, BRepr],
-                                                                          m: Migration[ARepr, B]): Migration[A, B] =
-    Migration.lift(a => {
-      println(s"Migrating using generic migration: $a")
-      val migrated = m.migrate(genA.to(a))
-      println(s"Migrated generic $a => $migrated")
-      migrated
-    })
+  implicit def coproductMigration[A, B, ARepr <: Coproduct, BRepr <: HList](implicit
+                                                                            genA: Generic.Aux[A, ARepr],
+                                                                            genB: LabelledGeneric.Aux[B, BRepr],
+                                                                            m: Migration[ARepr, B]): Migration[A, B] =
+    Migration.lift(a =>
+      m.migrate(genA.to(a))
+    )
 
   import shapeless.syntax.singleton._
 
@@ -79,31 +72,29 @@ object Migration {
     def empty: A
   }
 
-  implicit def labelledHListMonoid[K <: Symbol, H, T <: HList](
-                                                                implicit
-                                                                mT: Defaulter[T],
-                                                                selector: Selector.Aux[funs2.type, K, H]
-                                                              ): Defaulter[FieldType[K, H] :: T] = {
+  implicit def recordDefaulter[K <: Symbol, H, T <: HList](
+                                                            implicit
+                                                            mT: Defaulter[T],
+                                                            selector: Selector.Aux[funs2.type, K, H]
+                                                          ): Defaulter[FieldType[K, H] :: T] = {
 
 
     new Defaulter[FieldType[K, H] :: T] {
-      val empty = {
-        println("Trying to use labelled hlist empty monoid")
-        println(s"selector: ${selector(funs2)}")
+      val empty: ::[FieldType[K, H], FieldType[K, T]] = {
         field[K](selector(funs2)) :: field[K](mT.empty)
       }
     }
   }
 
-  implicit def hListMigration[A, ARepr <: HList, B, BRepr <: HList, Common <: HList, Added <: HList, Unaligned <: HList, Mapped <: HList](implicit
-                                                                                                                                          genA: LabelledGeneric.Aux[A, ARepr],
-                                                                                                                                          genB: LabelledGeneric.Aux[B, BRepr],
-                                                                                                                                          inter: hlist.Intersection.Aux[ARepr, BRepr, Common],
-                                                                                                                                          diff: hlist.Diff.Aux[BRepr, Common, Added],
-                                                                                                                                          defaulter: Defaulter[Added],
-                                                                                                                                          prepend: hlist.Prepend.Aux[Added, Common, Unaligned],
-                                                                                                                                          align: hlist.Align[Unaligned, BRepr]
-                                                                                                                                         ): Migration[A, B] =
+  implicit def productMigration[A, ARepr <: HList, B, BRepr <: HList, Common <: HList, Added <: HList, Unaligned <: HList, Mapped <: HList](implicit
+                                                                                                                                            genA: LabelledGeneric.Aux[A, ARepr],
+                                                                                                                                            genB: LabelledGeneric.Aux[B, BRepr],
+                                                                                                                                            inter: hlist.Intersection.Aux[ARepr, BRepr, Common],
+                                                                                                                                            diff: hlist.Diff.Aux[BRepr, Common, Added],
+                                                                                                                                            defaulter: Defaulter[Added],
+                                                                                                                                            prepend: hlist.Prepend.Aux[Added, Common, Unaligned],
+                                                                                                                                            align: hlist.Align[Unaligned, BRepr]
+                                                                                                                                           ): Migration[A, B] =
     Migration.lift {
       a =>
         genB.from(align(prepend(defaulter.empty, inter(genA.to(a)))))
@@ -112,11 +103,6 @@ object Migration {
 
   implicit val hNilDefaulter: Defaulter[HNil] = new Defaulter[HNil] {
     val empty: HNil.type = HNil
-  }
-
-
-  trait DoSomething[A] {
-    def apply(a: A): String
   }
 
 }
