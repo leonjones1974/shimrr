836456a857a58a9fad3056580acff75e122dd058
Leon Jones <leonj_24@yahoo.com>
2017-12-22 14:25:48 +0000

    Cleaning macro

--- src/main/scala/uk/camsw/shimmr/test/MigrationFreeSpec.scala
+++ src/main/scala/uk/camsw/shimmr/test/MigrationFreeSpec.scala
@@ -21,15 +21,12 @@ object MigrationFreeSpecMacros {
   def generateAnyToAny[A: c.WeakTypeTag](c: blackbox.Context): c.Expr[Any] = {
     import c.universe._
 
-    val x = c.weakTypeOf[A]
-    val subClasses = x.typeSymbol.asClass.knownDirectSubclasses
-    println(s"Found subclasses: ${subClasses}")
-    val scTypes = subClasses.map(_.asType)
-    println(s"With types: ${scTypes}")
+    val coproduct = c.weakTypeOf[A]
+    val productsT = coproduct.typeSymbol.asClass.knownDirectSubclasses.map(_.asType)
 
     val tests = for {
-      in <- scTypes
-      out <- scTypes
+      in <- productsT
+      out <- productsT
     } yield {
       q"""
          "Migrating from " + ${in.toString} + "~>" + ${out.toString} in {
@@ -46,22 +43,6 @@ object MigrationFreeSpecMacros {
          ..$tests
        """
     )
-
-    //    c.Expr(
-    //      q"""{
-    //         "Migrating" in {
-    //         val x: $x = Str1Str2("str1", "str2")
-    //
-    //         val xs: List[$x] = List(
-    //            Str1Str2("str1", "str2")
-    //         )
-    //         val z = xs.migrateTo[$inType]
-    //         println("z is: " + z)
-    //          true shouldBe false
-    //        }
-    //        }
-    //      """
-    //    )
   }
 
 }
