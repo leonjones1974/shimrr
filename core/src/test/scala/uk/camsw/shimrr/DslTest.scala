7f5fb8db3cb0122d89236e44f9a68192aaa3e143
Leon Jones <leonj_24@yahoo.com>
2017-12-29 01:59:48 +0000

    Add product level scoping to dsl test

--- core/src/test/scala/uk/camsw/shimrr/DslTest.scala
+++ core/src/test/scala/uk/camsw/shimrr/DslTest.scala
@@ -3,8 +3,8 @@ package uk.camsw.shimrr
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
 import shapeless.HNil
-import uk.camsw.shimrr.macros.migration
 import shapeless.syntax.singleton.mkSingletonOps
+import uk.camsw.shimrr.macros.migration
 
 class DslTest extends FreeSpec {
 
@@ -21,14 +21,24 @@ class DslTest extends FreeSpec {
               HNil
         }
 
+        @migration
+        object Str1Str2Rules extends Dsl[Str1Str2] {
+
+          override val fieldDefaults =
+              'intField1 ->> 51 ::
+              HNil
+        }
+
         import Str1Rules.exports._
         import syntax._
         import uk.camsw.shimrr.context.scoped._
-
+        import Str1Str2Rules.exports._
 
         Str1("str1").migrateTo[Str1] shouldBe Str1("str1")
         Str1("str1").migrateTo[Str1Str2] shouldBe Str1Str2("str1", "str2")
         Str1("str1").migrateTo[Str1Str2Int1] shouldBe Str1Str2Int1("str1", "str2", 25)
+
+        Str1Str2("str1", "str2").migrateTo[Str1Str2Int1] shouldBe Str1Str2Int1("str1", "str2", 51)
       }
     }
   }
